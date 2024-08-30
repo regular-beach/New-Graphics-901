cf066b82ef6c36aa0760caebdf77aee38be2f691
regular beach <renconger@gmail.com>
2024-08-30 11:59:54 -0700

    img bugssss

--- app.py
+++ app.py
@@ -78,3 +78,16 @@ if __name__ == '__main__':
 #if __name__ == '__main__':
   #app. run(host='0.0.0.0', port=3000)
     
+
+
+@app.context_processor
+def override_url_for():
+  return dict(url_for=dated_url_for)
+
+def dated_url_for(endpoint, **values):
+  if endpoint == 'static':
+    filename = values.get('filename', None)
+    if filename:
+      file_path = os.path.join(app.root_path, endpoint, filename)
+      values['q'] = int(os.stat(file_path).st_mtime)
+  return url_for(endpoint, **values)
